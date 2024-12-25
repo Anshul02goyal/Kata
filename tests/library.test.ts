138988c6a802490cdf3417e4d063d0610a6ec309
Anshul <anshu@Anshul.(none)>
2024-12-25 15:19:51 +0530

    Added tests for viewing available books

--- tests/library.test.ts
+++ tests/library.test.ts
@@ -1,53 +1,65 @@
-import {Library, Book} from "../src/library";
+import { Library, Book } from "../src/library";
 
-describe("Library Management System", () =>{
-    let library: Library;
+describe("Library Management System", () => {
+  let library: Library;
 
-    beforeEach(() =>{
-        library = new Library();
-    });
+  beforeEach(() => {
+    library = new Library();
+  });
 
-    test("should add a new book to the library", () =>{
-        const book: Book = {
-            ISBN: "123456",
-            title: "The Hobbit",
-            author: "Tolkien",
-            publicationYear: 1937
-        }
+  test("should add a new book to the library", () => {
+    const book: Book = {
+      ISBN: "123456",
+      title: "The Hobbit",
+      author: "Tolkien",
+      publicationYear: 1937,
+    };
 
-        library.addBook(book);
-    })
+    library.addBook(book);
 
-    test("should borrow an available book", () =>{
-        const book: Book = {
-            ISBN: "123456",
-            title: "The Hobbit",
-            author: "Tolkien",
-            publicationYear: 1937
-        };
+    expect(library.viewAvailbleBooks()).toEqual([
+      { ...book, isAvailable: true },
+    ]);
+  });
 
-        library.addBook(book);
-        library.borrowBook("123456");
-    });
+  test("should borrow an available book", () => {
+    const book: Book = {
+      ISBN: "123456",
+      title: "The Hobbit",
+      author: "Tolkien",
+      publicationYear: 1937,
+    };
 
-    test("should throw an error if the book is unavailable", () =>{
-        expect(() => library.borrowBook("9999")).toThrow("Book not available");
-    });
+    library.addBook(book);
+    library.borrowBook("123456");
 
-    test("should return a borrowed book", () =>{
-        const book: Book = {
-            ISBN: "123456",
-            title: "The Hobbit",
-            author: "Tolkien",
-            publicationYear: 1937
-        };
+    expect(library.viewAvailableBooks()).toEqual([]);
+  });
 
-        library.addBook(book);
-        library.borrowBook("123456");
-        library.returnBook("123456");
-    });
+  test("should throw an error if the book is unavailable", () => {
+    expect(() => library.borrowBook("9999")).toThrow("Book not available");
+  });
 
-    test("should throw an error if returning a book that is not borrowed", () =>{
-        expect(() => library.returnBook("1234")).toThrow("Book not found or already available");
-    })
-})
+  test("should return a borrowed book", () => {
+    const book: Book = {
+      ISBN: "123456",
+      title: "The Hobbit",
+      author: "Tolkien",
+      publicationYear: 1937,
+    };
+
+    library.addBook(book);
+    library.borrowBook("123456");
+    library.returnBook("123456");
+
+    expect(library.viewAvailableBooks()).toEqual([
+      { ...book, isAvailable: true },
+    ]);
+  });
+
+  test("should throw an error if returning a book that is not borrowed", () => {
+    expect(() => library.returnBook("1234")).toThrow(
+      "Book not found or already available"
+    );
+  });
+});
