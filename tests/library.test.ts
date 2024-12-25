362a54fe64a8ce8215146ca18e05d09a203b909e
Anshul <anshu@Anshul.(none)>
2024-12-25 15:06:30 +0530

    Added tests for borrowing books

--- tests/library.test.ts
+++ tests/library.test.ts
@@ -1,4 +1,4 @@
-import {Library} from "../src/library";
+import {Library, Book} from "../src/library";
 
 describe("Library Management System", () =>{
     let library: Library;
@@ -8,12 +8,30 @@ describe("Library Management System", () =>{
     });
 
     test("should add a new book to the library", () =>{
-        library.addBook({
+        const book: Book = {
             ISBN: "123456",
             title: "The Hobbit",
             author: "Tolkien",
             publicationYear: 1937
-        })
+        }
+
+        library.addBook(book);
     })
+
+    test("should borrow an available book", () =>{
+        const book: Book = {
+            ISBN: "123456",
+            title: "The Hobbit",
+            author: "Tolkien",
+            publicationYear: 1937
+        };
+
+        library.addBook(book);
+        library.borrowBook("123456");
+    });
+
+    test("should throw an error if the book is unavailable", () =>{
+        expect(() => library.borrowBook("9999")).toThrow("Book not available");
+    });
 
 })
